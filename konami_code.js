bcf57949d4da2a7fba8a9a8b961130335d522e5b
Kiki Dolan <cdolan323@gmail.com>
2018-08-22 00:18:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// We have an array of key codes (event.which)
+//We want to add an event listening somewhere so that when we press this, something happens
+//We need to keep track of where we are in the combo - meaning we need to start from the beginning if the user messes up 
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+code.addEventListener
 }
