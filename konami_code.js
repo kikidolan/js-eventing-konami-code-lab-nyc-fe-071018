9ca7abe97f4c6babeece44893e141b17f6161d34
Kiki Dolan <cdolan323@gmail.com>
2018-08-22 00:46:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+tracker = 0
+
+function onKeyDownHandler(event){
+   if (tracker === code.length) {
+    event.preventDefault()
+    return
+  }
+   if (event.which === code[tracker]) {
+    console.log('YAS!')
+    tracker ++
+    if (tracker === code.length - 1) {
+      alert ('BOOM!')
+  }
+}
+
 function init() {
   let tracker = 0
   // Write your JavaScript code inside the init() function
