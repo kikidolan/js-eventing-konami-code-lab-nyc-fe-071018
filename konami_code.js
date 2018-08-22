10f2214aaceb03d760f6d7360e32d0a952e6661d
Kiki Dolan <cdolan323@gmail.com>
2018-08-22 00:27:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,14 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-// We have an array of key codes (event.which)
-//We want to add an event listening somewhere so that when we press this, something happens
-//We need to keep track of where we are in the combo - meaning we need to start from the beginning if the user messes up 
 
 function init() {
   const tracker = 0
   // Write your JavaScript code inside the init() function
 const body = document.querySelector('body')
 body.addEventListener('keydown', (event) => {
-  if (event.which === code[tracker] ) {
+  if (event.which === code[tracker]) {
     console.log('YAS!')
     tracker ++
   } else {
