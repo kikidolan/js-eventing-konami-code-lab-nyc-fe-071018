4837c6d6892b42ad3937d033633b087f5dd8a8ad
Kiki Dolan <cdolan323@gmail.com>
2018-08-22 01:01:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let tracker = 0
 
 function onKeyDownHandler(event) {
+  console.log(tracker)
   const key = parseInt(event.detail || event.which)
   if (tracker === code.length) {
     window.alert("YAS!")
