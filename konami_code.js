6e97be34042690de12a007e21595602c5f49066d
Kiki Dolan <cdolan323@gmail.com>
2018-08-22 00:31:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
-  const tracker = 0
+  let tracker = 0
   // Write your JavaScript code inside the init() function
 const body = document.querySelector('body')
 body.addEventListener('keydown', (event) => {
